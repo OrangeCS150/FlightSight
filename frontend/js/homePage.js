4409866521d7c7468107e545d60c40c15bbd0267
Jonell Felix Santiago <jonellvsantiago045@gmail.com>
2026-02-12 23:55:04 -0800

    integrated safety the system

--- frontend/js/homePage.js
+++ frontend/js/homePage.js
@@ -6,4 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "flightRoute.html";
     });
   }
+
+  document.getElementById("safety-btn").addEventListener("click", function () {
+    window.location.href = "safety.html";
+  });
+
+  document.getElementById("route-btn").addEventListener("click", function () {
+    window.location.href = "route.html";
+  });
+
+  document.getElementById("demand-btn").addEventListener("click", function () {
+    window.location.href = "demand.html";
+  });
+
+  document.getElementById("saved-btn").addEventListener("click", function () {
+    window.location.href = "saved.html";
+  });
+
 });
